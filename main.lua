a4fc03efca3abb6b02e17c70f63ec36f1a6a40ef
Safebox36 <safebox36@gmail.com>
2020-12-08 00:41:59 +0000

    Improved Guards Patrol

--- main.lua
+++ main.lua
@@ -8,19 +8,22 @@ local function sqrDist(g, p)
         math.pow(math.abs(gPos.y - pPos.y), 2) +
         math.pow(math.abs(gPos.z - pPos.z), 2)
     )
-    print(dist)
     return dist
 end
 
 local function getNextPos(gK, gV)
-    local next = gV.cur + (1 * gV.dir)
-    print("Next: " .. next)
-    print(gV.path[next])
-    if (gV.path[next] == nil) then
-        gV.dir = gV.dir * -1
+    local next
+    if (gV.dir == 0) then
+        next = gV.cur + 1
+        if (gV.path[next] == nil) then
+            next = 1
+        end
+    else
         next = gV.cur + (1 * gV.dir)
-        print("Corrected Next: " .. next)
-        print(gV.path[next])
+        if (gV.path[next] == nil) then
+            gV.dir = gV.dir * -1
+            next = gV.cur + (1 * gV.dir)
+        end
     end
     gV.cur = next
     tes3.setAITravel{reference = gK, destination = tes3.getReference(gV.path[next]).position}
@@ -43,11 +46,16 @@ local function updateGuards()
     end
 end
 
+local function saveAndQuit()
+    mwse.saveConfig('aa_config', config)
+end
+
 local function init()
     print("===========")
     print("AA BEGIN...")
     event.register('cellChanged', initGuards)
     event.register('simulate', updateGuards)
+    event.register('save', saveAndQuit)
     print("AA SUCCESS")
     print("==========")
 end
